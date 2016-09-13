8c4c6a94a4a7dbbc8166f56c7a8e9511093f6fa0
wangchuny <wangchuny@wangchuny-PC.thunisoft.com>
2016-09-13 11:00:22 +0800

    修改了获取字段类型的sql，输出的字段类型是具体的类型而不是类型id

--- src/com/thunisoft/dbcompare/util/SQL.java
+++ src/com/thunisoft/dbcompare/util/SQL.java
@@ -8,6 +8,6 @@ public class SQL {
 	public final static String MYSQL_TABLENAMA = "{tablename}";
 	
 	public final static String SYBASE_GETTABLES_SQL = "SELECT name as TABLE_NAME,id as TABLE_ID FROM sysobjects WHERE type = 'U'";
-	public final static String SYBASE_GETCOLUMNS_SQL = "SELECT name as COLUMN_NAME,type as COLUMN_TYPE FROM syscolumns WHERE id = {id}";
+	public final static String SYBASE_GETCOLUMNS_SQL = "SELECT c.name as COLUMN_NAME,t.name as COLUMN_TYPE FROM syscolumns c,systypes t WHERE id = {id} AND  c.usertype = t.usertype";
 	public final static String SYBASE_TABLEID = "{id}"; 
 }
